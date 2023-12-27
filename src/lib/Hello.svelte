00f86ce284143b2a98165c9ee0a15180cb5cc77c
qiuzhanghua <qiuzhanghua@icloud.com>
2023-12-27 12:03:15 +0800

    feat: add lightswitch

--- src/lib/Hello.svelte
+++ src/lib/Hello.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+
+</script>
 <h2 class="h3">Hello world!</h2>
 <button type="button" class="btn variant-filled">Button</button>
+<LightSwitch />
